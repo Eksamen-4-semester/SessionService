fda6519a26c0009c8fa307225035d4619890e1f3
Thanushanb <Thanushanbs@gmail.com>
2026-05-22 12:18:32 +0200

    fixet forbindelsen

--- Models/Session.cs
+++ Models/Session.cs
@@ -2,32 +2,52 @@
 
 namespace SessionService.Models;
 
-public class Session //En session = En hold træning
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+public class Session
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("SessionId")]
     public int SessionId { get; set; }
-    
+
+    [BsonElement("SessionName")]
     public string SessionName { get; set; }
-    
+
+    [BsonElement("StartTime")]
     public DateTime StartTime { get; set; }
 
+    [BsonElement("EndTime")]
     public DateTime EndTime { get; set; }
 
-    public int InstructorId { get; set; } //Der kan kun være en instruktør på en hold træning
+    [BsonElement("InstructorId")]
+    public int InstructorId { get; set; }
 
+    [BsonElement("RoomId")]
     public int RoomId { get; set; }
 
+    [BsonElement("CurrentCapacity")]
     public int CurrentCapacity { get; set; }
-    
+
+    [BsonElement("MaxCapacity")]
     public int MaxCapacity { get; set; }
 
+    [BsonElement("Status")]
     public TeamSessionStatus Status { get; set; }
 }
+
 public enum TeamSessionStatus
 {
     Cancelled,
     Available,
     Full
 }
+
 
 //tip
 // Man kan søge for antal af tilmeldte i en session ved at søge
